b49061da071675342e461b970141321309ddca72
PSP-ElviraP <Elvirap@photoservicepros.com>
2018-07-24 19:24:09 +0200

    Added hot reloading

--- src/server/express.js
+++ src/server/express.js
@@ -2,8 +2,17 @@ import express from 'express';
 import path from 'path';
 
 const server = express();
+const webpack = require('webpack');
+const config = require('../../config/webpack.dev');
+const compiler = webpack(config);
+
+const webpackDevMiddleware = require('webpack-dev-middleware')(compiler, config.devServer);
+const webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
 const staticMiddleware = express.static('dist');
 
+
+server.use(webpackDevMiddleware);
+server.use(webpackHotMiddleware);
 server.use(staticMiddleware);
 
 server.listen(8080, () => {
