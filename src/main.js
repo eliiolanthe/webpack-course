8144158fce889bb91875746f0fbb4375c2332803
Elvira Paulin <eli.paulin@gmail.com>
2018-07-27 00:30:04 +0200

    Adding react

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 require('babel-runtime/regenerator');
+require('babel-register');
 require('webpack-hot-middleware/client?reload=true');
 require('./main.css');
+//require('./images/sky.jpg');
 require('./index.html');
+require('./app');
