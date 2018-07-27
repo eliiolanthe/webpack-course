9ed037cda60a1d563fa5f9e0b8d3c813ddb1a4e0
PSP-ElviraP <Elvirap@photoservicepros.com>
2018-07-27 19:33:08 +0200

    react hot reloading

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './counter';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-  <div>Hello from react! :D</div>,
-  document.getElementById('react-root')
-);
+function render(Component) {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('react-root')
+  );
+}
+
+render(Counter);
+
+if (module.hot) {
+  module.hot.accept('./counter.js', () => {
+    const NewCounter = require('./counter.js').default
+    render(NewCounter);
+  });
+}
---
+++ src/counter.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { hot } from 'react-hot-loader';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  climb() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>Counting: {this.state.count}</h2>
+        <button onClick={this.climb.bind(this)}>Click me</button>
+      </div>
+    )
+  }
+}
+
+export default hot(module)(Counter);
